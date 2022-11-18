2bb21ec89bd1cb82dd71a2aed61f2858ccf08295
posop1 <artur.yakhin52@gmail.com>
2022-11-18 17:46:46 +0500

    add 500 page

---
+++ pages/500.tsx
@@ -0,0 +1,14 @@
+import Layout from '../layouts/Layout'
+import s from '../styles/ErrorPage.module.scss'
+
+const ErrorPage500: React.FC = () => {
+  return (
+    <Layout title="500 - not found">
+      <div className={s.ErrorPage}>
+        <h1>500 - Not Found</h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default ErrorPage500
