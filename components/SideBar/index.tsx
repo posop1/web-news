8c08698e8beec07ffb602ea9014403d0c5ea5647
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-19 22:20:52 +0500

    add logo and title

--- components/SideBar/index.tsx
+++ components/SideBar/index.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image'
+import logo from '../../public/logo.svg'
 import s from './SideBar.module.scss'
 
 interface SideBarProps {}
 
 const SideBar: React.FC<SideBarProps> = () => {
-  return <div className={s.sidebar}>sidebar</div>
+  return (
+    <div className={s.sidebar}>
+      <div className={s.logo__container}>
+        <Image
+          src={logo}
+          alt="logo"
+          width="29"
+        />
+        <h1 className={s.title}>Aster News</h1>
+      </div>
+    </div>
+  )
 }
 
 export default SideBar
