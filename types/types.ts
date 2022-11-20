c5347b50d776a31b95583c406aa2fba5b955a8ff
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-20 12:19:51 +0500

    add routes

---
+++ types/types.ts
@@ -0,0 +1,7 @@
+import { IconType } from 'react-icons/lib'
+
+export interface IRoute {
+  route: string
+  name: string
+  component: IconType
+}
--- utils/constans.ts
+++ utils/constans.ts
@@ -1,4 +1,14 @@
-import { FiHome } from 'react-icons/fi'
+import { FiHome, FiPlay } from 'react-icons/fi'
+import { GiHealthNormal } from 'react-icons/gi'
+import { IoMedalOutline } from 'react-icons/io5'
+import { MdOutlineBusinessCenter, MdOutlineHealthAndSafety } from 'react-icons/md'
+import { IRoute } from '../types/types'
 
-export const routes = [
-  { route: '/', name: 'Top News', component: FiHome }]
+export const routes: IRoute[] = [
+  { route: '/', name: 'Top News', component: FiHome },
+  { route: '/business', name: 'Business', component: MdOutlineBusinessCenter },
+  { route: '/health', name: 'Health', component: MdOutlineHealthAndSafety },
+  { route: '/entertainment', name: 'Entertainment', component: FiPlay },
+  { route: '/sports', name: 'Sports', component: IoMedalOutline },
+  { route: '/covid', name: 'Covid', component: GiHealthNormal }
+]
