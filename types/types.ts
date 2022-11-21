c057029b3b6dae073d9f825e74f9c556b4b48eb4
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-21 17:46:30 +0500

    add types for news

--- types/types.ts
+++ types/types.ts
@@ -4,4 +4,26 @@ export interface IRoute {
   route: string
   name: string
   component: IconType
+  pageName: string
+}
+
+export interface INews {
+  status: string
+  totalResults: number
+  articles: IArticle[]
+}
+
+export interface IArticle {
+  author?: string
+  content: string
+  description: string
+  source: ISource
+  title: string
+  url: string
+  urlToImage: string
+}
+
+export interface ISource {
+  id: string
+  name: string
 }
