a3feee1d390bedff0af9328eac16d65a82816f5f
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-21 18:14:04 +0500

    add dynamic page

--- pages/[pageName].tsx
+++ pages/[pageName].tsx
@@ -1,28 +1,41 @@
+import { ucs2 } from 'punycode'
+import { GetServerSideProps } from 'next'
 import NewsList from '../components/NewsList'
 import Layout from '../layouts/Layout'
+import { INews } from '../types/types'
 
-interface NewsPagesProps {}
+interface NewsPagesProps {
+  news: INews
+  pageName: string
+}
 
-export const getServerSideProps = async (context) => {
-  console.log(context)
-
-  const { article } = context.params
+export const getServerSideProps: any = async (context: any) => {
+  const { pageName } = context.params
   const res = await fetch(
-    `https://newsapi.org/v2/everything?q=${article}&apiKey=64a74bc147c14569bd3b46602f5cde5f`
+    `https://newsapi.org/v2/everything?q=${pageName}&apiKey=64a74bc147c14569bd3b46602f5cde5f`
   )
   const data = await res.json()
 
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
+
   return {
-    props: { news: data }
+    props: { news: data, pageName: pageName }
   }
 }
 
-const NewsPages: React.FC<NewsPagesProps> = ({ news }) => {
-  console.log(news)
+const NewsPages: React.FC<NewsPagesProps> = ({ news, pageName }) => {
+  const title = pageName.charAt(0).toUpperCase() + pageName.slice(1)
 
   return (
     <Layout>
-      <NewsList title="te" />
+      <NewsList title={title} />
     </Layout>
   )
 }
