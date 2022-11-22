b17f85ef51e14dc4239addd0a30aa139bd14ddf4
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-22 12:11:14 +0500

    add news props

--- components/NewsList/index.tsx
+++ components/NewsList/index.tsx
@@ -1,10 +1,12 @@
+import { INews } from '../../types/types'
 import s from './NewsList.module.scss'
 
 interface NewsListProps {
   title: string
+  news: INews
 }
 
-const NewsList: React.FC<NewsListProps> = ({ title }) => {
+const NewsList: React.FC<NewsListProps> = ({ title, news }) => {
   return (
     <div className={s.news__list}>
       <h1>{title}</h1>
--- pages/[pageName].tsx
+++ pages/[pageName].tsx
@@ -14,7 +14,7 @@ export const getServerSideProps: any = async (context: any) => {
   const { pageName } = context.params
 
   const { data } = await axios.get(
-    `https://newsapi.org/v2/everything?q=${pageName}&apiKey=64a74bc147c14569bd3b46602f5cde5f`
+    `https://newsapi.org/v2/everything?q=${pageName}&language=ru&apiKey=64a74bc147c14569bd3b46602f5cde5f`
   )
 
   if (!data) {
@@ -33,10 +33,14 @@ export const getServerSideProps: any = async (context: any) => {
 
 const NewsPages: React.FC<NewsPagesProps> = ({ news, pageName }) => {
   const title = pageName.charAt(0).toUpperCase() + pageName.slice(1)
+  console.log(news)
 
   return (
     <Layout>
-      <NewsList title={title} />
+      <NewsList
+        title={title}
+        news={news}
+      />
     </Layout>
   )
 }
