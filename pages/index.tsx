386d61b1478d1335004496035d9ac91eaf3a74ea
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-22 16:37:21 +0500

    fetch news in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,36 @@
+import axios from 'axios'
 import { FC } from 'react'
 import NewsList from '../components/NewsList'
 import Layout from '../layouts/Layout'
+import { INews } from '../types/types'
 
-const Home: FC = () => {
+interface HomePageProps {
+  news: INews
+}
+
+export const getStaticProps = async () => {
+  const { data } = await axios.get(
+    'https://newsapi.org/v2/top-headlines?country=ru&pageSize=20&apiKey=64a74bc147c14569bd3b46602f5cde5f'
+  )
+
+  if (!data) {
+    return {
+      notFound: true
+    }
+  }
+
+  return {
+    props: { news: data }
+  }
+}
+
+const Home: FC<HomePageProps> = ({ news }) => {
   return (
-    <Layout>
-      <NewsList title="Test" />
+    <Layout title="Top News">
+      <NewsList
+        title="Top News"
+        news={news}
+      />
     </Layout>
   )
 }
