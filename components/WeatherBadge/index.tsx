2c1f1f7b4794c7b11a1519fb88a828825502ecdc
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-24 15:48:50 +0500

    add weather badge (finally)

--- components/WeatherBadge/index.tsx
+++ components/WeatherBadge/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { IWeather } from '../../types/types'
@@ -12,7 +13,7 @@ const WeatherBadge: React.FC = () => {
       const lon: number = 76.54337
       const API_key = 'ffae1cfd3f9b0d3b0c574eacf2702d6a'
       const { data } = await axios.get<IWeather>(
-        `https://api.openweathermap.org/data/2.5/weather?lang=ru&lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lang=ru&lat=${lat}&lon=${lon}&units=metric&appid=${API_key}`
       )
       setWeather(data)
     }
@@ -21,7 +22,33 @@ const WeatherBadge: React.FC = () => {
 
   console.log(weather)
 
-  return <div className={s.WeatherBadge}></div>
+  return (
+    <div className={s.WeatherBadge}>
+      <div className={s.country__container}>
+        <p>
+          {weather?.name}, {weather?.sys.country}
+        </p>
+      </div>
+      <hr className={s.hr} />
+      <div className={s.weather__container}>
+        <div>
+          <p>
+            {weather?.weather[0].description &&
+              weather?.weather[0].description.charAt(0).toUpperCase() +
+                weather?.weather[0].description.slice(1)}
+          </p>
+          <span>{weather?.main.temp.toString().slice(0, 3)}</span>
+        </div>
+        <img
+          src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}.png`}
+          alt="weather"
+        />
+      </div>
+      <div className={s.temp__type__container}>
+        <p>{weather?.wind.speed} м/c</p>
+      </div>
+    </div>
+  )
 }
 
 export default WeatherBadge
