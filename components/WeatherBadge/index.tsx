eaf845a7ef6c91b16245868b335afb8463cf4d63
Artur Yakhin <artur.yakhin52@gmail.com>
2022-11-24 15:09:52 +0500

    add types for weather fetch

--- components/WeatherBadge/index.tsx
+++ components/WeatherBadge/index.tsx
@@ -1,17 +1,18 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { IWeather } from '../../types/types'
 import s from './WeatherBadge.module.scss'
 
 const WeatherBadge: React.FC = () => {
-  const [weather, setWeather] = useState()
+  const [weather, setWeather] = useState<IWeather>()
 
   useEffect(() => {
     const fetchWeather = async () => {
-      const lat = 60.94312
-      const lon = 76.54337
+      const lat: number = 60.94312
+      const lon: number = 76.54337
       const API_key = 'ffae1cfd3f9b0d3b0c574eacf2702d6a'
-      const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?lang=ru&lat=${lat}&lon=${lon}&appid=${API_key}`
+      const { data } = await axios.get<IWeather>(
+        `https://api.openweathermap.org/data/2.5/weather?lang=ru&lat=${lat}&lon=${lon}&appid=${API_key}&units=metric`
       )
       setWeather(data)
     }
